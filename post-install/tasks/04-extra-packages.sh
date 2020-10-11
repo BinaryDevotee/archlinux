0c4ed64d7a6bdeffb00b33b6fd831a7775fd92d5
BinaryDevotee <athila.m.mattos@gmail.com>
2020-10-11 23:35:41 +0200

    Fixes

--- post-install/tasks/04-extra-packages.sh
+++ post-install/tasks/04-extra-packages.sh
@@ -6,6 +6,7 @@ source files/vars
 
 pacman --sync --refresh --needed --noconfirm $pkg_list
 
+echo 'Configuring Z-shell'
 homectl activate $user_name
 wget -q -O /home/$user_name/.zshrc       https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 wget -q -O /home/$user_name/.zshrc.local https://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
@@ -15,10 +16,12 @@ homectl update $user_name --shell=/usr/bin/zsh
 cat files/alias >> /home/$user_name/.zshrc.local
 homectl deactivate $user_name
 
+echo 'Installing Starship'
 wget -q -O /tmp/starship-latest.tar.gz https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz
 tar -xf /tmp/starship-latest.tar.gz -C /usr/local/bin/
 rm /tmp/starship-latest.tar.gz
 
+echo 'Configuring firewall'
 systemctl enable --now ufw
 ufw enable
 ufw allow nfs
