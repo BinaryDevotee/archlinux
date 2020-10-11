5cee57ba01d64802bae12d0d82578b8cf8bb5bbb
BinaryDevotee <athila.m.mattos@gmail.com>
2020-10-11 23:01:58 +0200

    Fixes

--- install/profiles/profile-01.sh
+++ install/profiles/profile-01.sh
@@ -8,101 +8,52 @@ set -e -u
 echo "Available block devices:" && echo "" && lsblk && echo ""
 read -p "Specify the block device for the installation [i.e: /dev/sda]: " pv
 
-wipe_disk () {
-    wipefs --all --force $pv && blkdiscard --force $pv
-}
-wipe_disk
+wipefs -a -f $pv && blkdiscard -f $pv
 
-partition_disk () {
-    parted --script $pv \
-    mklabel gpt \
-    mkpart primary 1MiB 301MiB \
-    mkpart primary 301MiB 201GiB \
-    name 1 ARCH_BOOT \
-    name 2 ARCH_OS \
-    set 1 boot on \
-    align-check optimal 1 \
-    align-check optimal 2
+parted -s $pv \
+mklabel gpt \
+mkpart primary 1MiB 301MiB \
+mkpart primary 301MiB 201GiB \
+name 1 ARCH_BOOT \
+name 2 ARCH_OS \
+set 1 boot on \
+align-check optimal 1 \
+align-check optimal 2
 
-    udevadm settle && sync
-}
-partition_disk
+udevadm settle && sync
 
-format_partitions () {
-    mkfs.fat -F32 /dev/disk/by-partlabel/ARCH_BOOT -n ARCH_BOOT
-    mkfs.f2fs -f /dev/disk/by-partlabel/ARCH_OS -l ARCH_OS
-    mount -L ARCH_OS /mnt && mkdir -p /mnt/boot
-    mount -L ARCH_BOOT /mnt/boot 
-}
-format_partitions
+mkfs.fat -F32 /dev/disk/by-partlabel/ARCH_BOOT -n ARCH_BOOT
+mkfs.f2fs -f /dev/disk/by-partlabel/ARCH_OS -l ARCH_OS
+mount -L ARCH_OS /mnt && mkdir -p /mnt/boot
+mount -L ARCH_BOOT /mnt/boot 
 
-install_archlinux () {
-    pacstrap /mnt base base-devel linux linux-firmware intel-ucode neovim networkmanager iwd openssh f2fs-tools
-}
-install_archlinux
+pacstrap /mnt base base-devel linux linux-firmware intel-ucode neovim networkmanager iwd openssh f2fs-tools
+genfstab -L /mnt >> /mnt/etc/fstab
 
-generate_fstab () {
-    genfstab -L /mnt >> /mnt/etc/fstab
-}
-generate_fstab
+sed -e '52s/udev/systemd/g' -i /mnt/etc/mkinitcpio.conf
+arch-chroot /mnt mkinitcpio --allpresets
+arch-chroot /mnt passwd
 
-update_initramfs () {
-    sed -e '52s/udev/systemd/g' -i /mnt/etc/mkinitcpio.conf
-    arch-chroot /mnt mkinitcpio --allpresets
-}
-update_initramfs
+arch-chroot /mnt bootctl --path=/boot install
+cat <<EOF > /mnt/boot/loader/loader.conf
+default arch
+timeout 0
+console-mode keep
+editor no
+EOF
 
-root_passwd () {
-    arch-chroot /mnt passwd
-}
-root_passwd
+cat <<EOF > /mnt/boot/loader/entries/arch.conf
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux.img
+options root=LABEL=ARCH_OS rw
+options intel_iommu=on
+EOF
+arch-chroot /mnt bootctl --path=/boot update
 
-bootloader () {
-    install_bootloader () {
-        arch-chroot /mnt bootctl --path=/boot install
-    }
-    install_bootloader
+cp -r ../post-install /mnt/root
+cp -r ../roles /mnt/root
 
-    loader_conf () {
-        printf '%s\n' > /mnt/boot/loader/loader.conf \
-        'default arch' \
-        'timeout 0' \
-        'console-mode keep' \
-        'editor no'
-    }
-    loader_conf
-
-    entries_conf () {
-        printf '%s\n' > /mnt/boot/loader/entries/arch.conf \
-        'title   Arch Linux' \
-        'linux   /vmlinuz-linux' \
-        'initrd  /intel-ucode.img' \
-        'initrd  /initramfs-linux.img' \
-        'options root=LABEL=ARCH_OS rw' \
-	'options intel_iommu=on'
-    }
-    entries_conf
-
-    update_bootloader () {
-        arch-chroot /mnt bootctl --path=/boot update
-    }
-    update_bootloader
-}
-bootloader
-
-ps_scripts () {
-    cp -r ../post-install /mnt/root
-    cp -r ../roles /mnt/root
-}
-ps_scripts
-
-enable_services () {
-    systemctl enable systemd-homed
-}
-enable_services
-
-finish_install () {
-    umount -R /mnt
-    systemctl poweroff
-}
-finish_install
+umount -R /mnt
+systemctl poweroff
